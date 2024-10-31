97f0d22a8091597f51fa9585ee70daefaf510e77
Shehroz Hanif <shehrozhanif54@gmail.com>
2024-10-30 21:53:34 -0700

    update

--- src/components/pg.tsx
+++ src/components/pg.tsx
@@ -22,7 +22,7 @@ function Pg() {
     if (charAllowed) str += "!@#$%^&*()-_=+\\|[]{};:/?.>";
 
     for (let i = 1; i < length; i++) {
-      let char = Math.floor(Math.random() * str.length + 1);
+      const char = Math.floor(Math.random() * str.length + 1);
       pass += str.charAt(char);
     }
 
